25f0c42a03532248c8568e0997b861ebcadab28a
Alexandru-Sever Horin <alex.sever.h@gmail.com>
2020-01-26 17:54:16 +0200

    Fix run script

--- run.sh
+++ run.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
 
-rsync -aAv --progress --delete `pwd`/ pi@10.3.14.102:~/HomeSensorHub/
-ssh -t pi@raspberry-sensor-dev python3 /home/pi/HomeSensorHub/EnvironmentalSensor.py
+rsync -aAv --progress --delete `pwd`/ pi@raspberry-sensor-dev:~/HomeSensorHub_$USER/
+
+case "$1" in
+    "env")
+        ssh -t pi@raspberry-sensor-dev python3 /home/pi/HomeSensorHub$USER/EnvironmentalSensor.py
+        ;;
+    "motion")
+        ssh -t pi@raspberry-sensor-dev python3 /home/pi/HomeSensorHub_$USER/MotionSensor.py
+        ;;
+    "light")
+        ssh -t pi@raspberry-sensor-dev python3 /home/pi/HomeSensorHub_$USER/LightSensor.py
+        ;;
+    *)
+        exit 4
+        ;;
+esac
