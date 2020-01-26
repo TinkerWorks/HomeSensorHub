3b9054edfeb8a6da3432dfe5dc21d2f5c2e4fcd0
Maria-Minerva Vonica <maria.vonica98@gmail.com>
2020-01-26 18:20:50 +0200

    Fixed typo in requirements.txt and (YES AND) added requirements install in the script.

--- run.sh
+++ run.sh
@@ -2,6 +2,8 @@
 
 rsync -aAv --progress --delete `pwd`/ pi@raspberry-sensor-dev:~/HomeSensorHub_$USER/
 
+ssh -t pi@raspberry-sensor-dev pip3 install -r /home/pi/HomeSensorHub_$USER/requirements.txt
+
 case "$1" in
     "env")
         ssh -t pi@raspberry-sensor-dev python3 /home/pi/HomeSensorHub_$USER/EnvironmentalSensor.py
