a102b350dc598773a7362766326ab86f9c518309
Maria-Minerva Vonica <maria.vonica98@gmail.com>
2020-01-26 18:01:36 +0200

    Fixed run.sh directory.

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ rsync -aAv --progress --delete `pwd`/ pi@raspberry-sensor-dev:~/HomeSensorHub_$U
 
 case "$1" in
     "env")
-        ssh -t pi@raspberry-sensor-dev python3 /home/pi/HomeSensorHub$USER/EnvironmentalSensor.py
+        ssh -t pi@raspberry-sensor-dev python3 /home/pi/HomeSensorHub_$USER/EnvironmentalSensor.py
         ;;
     "motion")
         ssh -t pi@raspberry-sensor-dev python3 /home/pi/HomeSensorHub_$USER/MotionSensor.py
