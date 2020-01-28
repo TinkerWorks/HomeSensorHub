89eb7e1cc91372c77a42bc5508629ced21975e96
Maria-Minerva Vonica <maria.vonica98@gmail.com>
2020-01-28 22:11:31 +0200

    Updated script in order to be able to run the modules as scripts as well.

--- run.sh
+++ run.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 
-rsync -aAv --progress --delete `pwd`/ pi@raspberry-sensor-dev:~/HomeSensorHub_$USER/
+rsync -aAv --progress --delete "$(pwd)/" pi@raspberry-sensor-dev:~/HomeSensorHub_$USER/
 
 # ssh -t pi@raspberry-sensor-dev pip3 install -r /home/pi/HomeSensorHub_$USER/requirements.txt
 
 case "$1" in
+    "main")
+        ssh -t pi@raspberry-sensor-dev PYTHONPATH=/home/pi/HomeSensorHub_$USER python3 /home/pi/HomeSensorHub_$USER/main.py
+        ;;
     "env")
-        ssh -t pi@raspberry-sensor-dev python3 /home/pi/HomeSensorHub_$USER/main.py
+        ssh -t pi@raspberry-sensor-dev PYTHONPATH=/home/pi/HomeSensorHub_$USER python3 /home/pi/HomeSensorHub_$USER//sensors/environmental_sensor.py
         ;;
     "motion")
-        ssh -t pi@raspberry-sensor-dev python3 /home/pi/HomeSensorHub_$USER/sensors/motion_sensor.py
+        ssh -t pi@raspberry-sensor-dev PYTHONPATH=/home/pi/HomeSensorHub_$USER python3 /home/pi/HomeSensorHub_$USER/sensors/motion_sensor.py
         ;;
     "light")
-        ssh -t pi@raspberry-sensor-dev python3 /home/pi/HomeSensorHub_$USER/sensors/light_sensor.py
+        ssh -t pi@raspberry-sensor-dev PYTHONPATH=/home/pi/HomeSensorHub_$USER python3 /home/pi/HomeSensorHub_$USER/sensors/light_sensor.py
         ;;
     *)
         exit 4
