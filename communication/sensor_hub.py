d284eb8d5eaf13cd978247b47210dcdb7b8d1d42
Maria-Minerva Vonica <maria.vonica98@gmail.com>
2020-01-29 20:36:58 +0200

    Implemented communication between the sensor hub and mqtt. Works.

--- communication/sensor_hub.py
+++ communication/sensor_hub.py
@@ -17,6 +17,9 @@ class SensorHub:
         self.all_sensors = [self.environmental_sensors]
         # TODO use getattr to iterate only over the sensors instead of hardcoding them into a list.
 
+    def get_data(self):
+        return self.data
+
     def collect_data_from(self, sensor_type) -> None:
         """
         Since all of the types (eg. environmental, motion, light) can have multiple sensors, over a call we get the data
@@ -24,10 +27,11 @@ class SensorHub:
         """
         for sensor in sensor_type.sensors:
             data = sensor.get_data()
-            print(data)
             self.data.update(data)
 
-    def collect_all_data(self):
+    def collect_all_data(self) -> None:
+        # TODO Fix the multiple sensors data. When a sensor type has multiple sensors, it actually overwrites the last
+        # value rather than having different values for different sensors
         for sensor in self.all_sensors:
             self.collect_data_from(sensor)
 
@@ -36,8 +40,8 @@ if __name__ == "__main__":
     se = SensorHub()
 
     while True:
-        # se.collect_data_from(se.environmental_sensors)
         se.collect_all_data()
-        # print(se.data)
+        data = se.get_data()
+        print(data)
 
         time.sleep(1)
--- main.py
+++ main.py
@@ -1,6 +1,16 @@
+import time
+
 from communication.data_sender import DataSender
 from communication.sensor_hub import SensorHub
 
 if __name__ == "__main__":
     sh = SensorHub()
     ds = DataSender()
+
+    while True:
+        sh.collect_all_data()
+        data = sh.get_data()
+
+        for topic, value in data.items():
+            ds.send_current(value, topic)
+        time.sleep(2)
