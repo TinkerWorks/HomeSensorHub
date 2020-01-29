380826d9eb60ac13d1e7ecd82c6d4a23e3fd13cd
Maria-Minerva Vonica <maria.vonica98@gmail.com>
2020-01-29 21:50:01 +0200

    Refactored the sensor hub and treated the case for multiple sensors for one type.

--- sensors/sensor_hub.py
+++ sensors/sensor_hub.py
@@ -1,27 +1,35 @@
 import time
 
-import board
-import busio
+from sensors.environmental_sensor import EnvironmentalSensor
 
 
 class SensorMonitor:
+    """
+    Class which will hold the information of all the types of sensors and their data. This will represent the data
+    communicator.
+    """
 
     ENVIRONMENTAL_SENSORS = None
-    MOTION_SENSORS = None
 
     def __init__(self):
         self.data = {}
-        self.ENVIRONMENTAL_SENSORS = EnvironmentalSensors()
-        # TODO self.MOTION_SENSORS = MovementSensors()\
+        self.ENVIRONMENTAL_SENSORS = EnvironmentalSensor()
 
-    def collect_data(self):
-        while True:
-            environmental_data = self.ENVIRONMENTAL_SENSORS.collect_data()
-            # TODO motion_data = self.MOTION_SENSORS.collect_data()
-
-            self.data.update(environmental_data)
-            # TODO self.data.update(motion_data)
-
-            time.sleep(2)
+    def collect_data(self, sensor_type) -> None:
+        """
+        Since all of the types (eg. environmental, motion, light) can have multiple sensors, over a call we get the data
+        from all the type's sensors, for now.
+        """
+        for sensor in sensor_type.sensors:
+            data = sensor.get_data()
+            self.data.update(data)
 
 
+if __name__ == "__main__":
+    se = SensorMonitor()
+
+    while True:
+        se.collect_data(se.ENVIRONMENTAL_SENSORS)
+        print(se.data)
+
+        time.sleep(1)
