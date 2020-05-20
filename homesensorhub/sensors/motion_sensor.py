52cea44cb2c12098b23f7c9a00feceee7c00d77e
Alexandru-Sever Horin <alex.sever.h@gmail.com>
2020-05-20 22:20:28 +0300

    [MOTION] Add state machine tests
    Mock the RPi GPIO class to simulate motion sensor transitions.
    Test callbacks that gives us events on transition.

--- homesensorhub/sensors/motion_sensor.py
+++ homesensorhub/sensors/motion_sensor.py
@@ -1,5 +1,7 @@
 #!/bin/env python3
 
+import logging
+
 from time import sleep
 import RPi.GPIO as GPIO
 
@@ -8,25 +10,32 @@ class MotionSensor:
 
     def __init__(self, gpio = 7, callback = None):
         self.MOTION_GPIO = gpio
-        self.callback = None
+        self.callback = callback
+        self.motion = False
+        self.logger = logging.getLogger(__name__)
 
     def initialize(self):
         GPIO.setmode(GPIO.BOARD)
         GPIO.setup(self.MOTION_GPIO, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
         MotionSensor.ms_dict[self.MOTION_GPIO] = self
-        GPIO.add_event_detect(self.MOTION_GPIO, GPIO.BOTH, callback=MotionSensor.motionChangedStatic)
+        GPIO.add_event_detect(self.MOTION_GPIO, GPIO.BOTH, callback=MotionSensor.__motionChangedStatic)
 
-    def motionChangedStatic(gpio):
-        MotionSensor.ms_dict[gpio].motionChanged(gpio)
+    def __motionChangedStatic(gpio):
+        MotionSensor.ms_dict[gpio].__motionChanged(gpio)
 
-    def motionChanged(self, gpio):
+    def __motionChanged(self, gpio):
         if GPIO.input(self.MOTION_GPIO):
-            print ("Motion Started %d" % gpio)
-            motion = 1
+            next_motion = True
+            self.logger.debug ("Motion Started %d" % gpio)
         else:
-            print ("Motion Ended %d" % gpio)
-            motion = 0
+            next_motion = False
+            self.logger.debug ("Motion Ended %d" % gpio)
 
-        if(self.callback is not None):
-            self.callback(motion)
+        if(self.motion == next_motion):
+            self.logger.warning("Motion Transitioned from {} to {}".format(self.motion, next_motion))
+        else:
+            self.motion = next_motion
+
+            if(self.callback is not None):
+                self.callback(self.motion)
