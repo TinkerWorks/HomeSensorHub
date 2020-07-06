2f83fbfe2a062a3154ae28a9be8d3dcf9d4cecb0
Maria-Minerva Vonica <maria.vonica98@gmail.com>
2020-07-06 20:50:52 +0300

    Deleted not yet necessary integration test.

--- tests/test_it_all.py
+++
@@ -1,7 +0,0 @@
-from communication.sensor_sink import SensorSink
-import unittest
-
-class TestItAll(unittest.TestCase):
-    def test_it_all(self):
-        sensor_sink = SensorSink()
-        sensor_sink.sink(interval=3)
