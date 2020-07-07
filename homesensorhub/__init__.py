86d56f7e07067a48a75ff835f6126f8d0297662d
Maria-Minerva Vonica <maria.vonica98@gmail.com>
2020-07-07 16:50:18 +0300

    Changed the entry point of the application in __main__.
    Since __init__ is a sub module identifier.

--- homesensorhub/__init__.py
+++ homesensorhub/__init__.py
@@ -1,6 +1,1 @@
-"""Entry point for the application."""
-from communication.sensor_sink import SensorSink
-
-if __name__ == "__main__":
-    sensor_sink = SensorSink()
-    sensor_sink.sink_and_send(3)
+"""Submodule idenfifier."""
