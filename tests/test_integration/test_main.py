43228a982157d68fb0a62bebed31390ed6bedec4
Maria-Minerva Vonica <maria.vonica98@gmail.com>
2020-08-26 18:55:06 +0300

    Run main for 5 seconds in a test.
    Iei.

--- tests/test_integration/test_main.py
+++ tests/test_integration/test_main.py
@@ -1,8 +1,12 @@
 import unittest
 import subprocess
+import time
+import signal
 
 
 class TestMain(unittest.TestCase):
 
     def test_main(self):
-        subprocess.call(["/usr/bin/python3", "homesensorhub/__main__.py"])
+        proc = subprocess.Popen(["/usr/bin/python3", "homesensorhub/__main__.py"])
+        time.sleep(5)
+        proc.send_signal(signal.SIGINT)
