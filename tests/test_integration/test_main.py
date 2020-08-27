3ce9e746aafeade0b8d68d0f45dbbf338c4b162a
Maria-Minerva Vonica <maria.vonica98@gmail.com>
2020-08-27 19:47:22 +0300

    Fixed main test integration.

--- tests/test_integration/test_main.py
+++ tests/test_integration/test_main.py
@@ -6,7 +6,20 @@ import signal
 
 class TestMain(unittest.TestCase):
 
+    TIMEOUT = 30
+
     def test_main(self):
+        """Test that main runs for five seconds without crashing."""
         proc = subprocess.Popen(["/usr/bin/python3", "homesensorhub/__main__.py"])
         time.sleep(5)
+
         proc.send_signal(signal.SIGINT)
+        timeout = self.TIMEOUT
+        while not proc.poll() and timeout >= 0:
+            time.sleep(0.1)
+            timeout -= 1
+
+        if not proc.poll():
+            proc.terminate()
+
+        self.assertEqual(0, proc.returncode)
