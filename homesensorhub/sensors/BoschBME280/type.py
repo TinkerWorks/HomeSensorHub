8455b01de873bae4deb0e5a610a342d604141723
Alexandru-Sever Horin <alex.sever.h@gmail.com>
2020-09-01 21:14:24 +0300

    Add FileLocks to I2C sensors
    Avoiding bad values when sensors are read from multiple processes

--- homesensorhub/sensors/BoschBME280/type.py
+++ homesensorhub/sensors/BoschBME280/type.py
@@ -1,4 +1,5 @@
 from sensors.sensor_types import Humidity, Pressure, Altitude, Temperature
+from filelock import FileLock
 
 
 class BoschBME280Type:
@@ -8,6 +9,7 @@ class BoschBME280Type:
     def __init__(self, sensor):
         """Initialise the BoschBME280 type with the physical sensor."""
         self._sensor = sensor
+        self._lock = FileLock("/var/tmp/hsh_" + self.SENSOR_NAME + ".lock")
 
     def get_sensor_name(self):
         return self.SENSOR_NAME
@@ -28,7 +30,8 @@ class HumidityBoschBME280(BoschBME280Type, Humidity):
         Humidity.__init__(self, send_payload_callback)
 
     def get_sensor_value(self) -> int:
-        return self._sensor.humidity
+        with self._lock:
+            return self._sensor.humidity
 
     def get_sensor_measure(self) -> str:
         return HumidityBoschBME280.MEASURE
@@ -42,7 +45,8 @@ class PressureBoschBME280(BoschBME280Type, Pressure):
         Pressure.__init__(self, send_payload_callback)
 
     def get_sensor_value(self) -> float:
-        return self._sensor.pressure
+        with self._lock:
+            return self._sensor.pressure
 
     def get_sensor_measure(self) -> str:
         return PressureBoschBME280.MEASURE
@@ -56,7 +60,8 @@ class AltitudeBoschBME280(BoschBME280Type, Altitude):
         Altitude.__init__(self, send_payload_callback)
 
     def get_sensor_value(self) -> int:
-        return self._sensor.altitude
+        with self._lock:
+            return self._sensor.altitude
 
     def get_sensor_measure(self) -> str:
         return AltitudeBoschBME280.MEASURE
@@ -70,7 +75,8 @@ class TemperatureBoschBME280(BoschBME280Type, Temperature):
         Temperature.__init__(self, send_payload_callback)
 
     def get_sensor_value(self) -> float:
-        return self._sensor.temperature
+        with self._lock:
+            return self._sensor.temperature
 
     def get_sensor_measure(self) -> str:
         return TemperatureBoschBME280.MEASURE
