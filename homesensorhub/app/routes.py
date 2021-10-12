c6d7fee6dfd0bc4e35d343d62b864bd5c6d9b4ce
Maria-Minerva Vonica <maria.vonica98@gmail.com>
2021-10-12 17:44:30 +0300

    Added base page.

--- homesensorhub/app/routes.py
+++ homesensorhub/app/routes.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
+from flask import render_template
 from app import app
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "HomeSensorHub configuration page."
+    user = {'username': 'Min'}
+    return render_template('index.html', user=user)
