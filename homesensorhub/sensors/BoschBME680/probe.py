02feb658ae9a7eb662d11a7a04b6168f20746f16
Alexandru-Sever Horin <alex.sever.h@tinker.works>
2023-01-05 20:47:44 +0200

    Remove duplicated lock code

--- homesensorhub/sensors/BoschBME680/probe.py
+++ homesensorhub/sensors/BoschBME680/probe.py
@@ -4,61 +4,49 @@ from homesensorhub.sensors.BoschBME680.type import AltitudeBoschBME680
 from homesensorhub.sensors.BoschBME680.type import HumidityBoschBME680
 from homesensorhub.sensors.BoschBME680.type import PressureBoschBME680
 from homesensorhub.sensors.BoschBME680.type import GasBoschBME680
+from homesensorhub.sensors.probe import Probe
 
-from filelock import FileLock
 import adafruit_bme680
 import busio
 import board
-import os
 
 from homesensorhub.utils import logging
 logger = logging.getLogger(__name__)
 
 
-class ProbeBoschBME680():
+class ProbeBoschBME680(Probe):
     """Class which implements probing for BME680 sensor."""
 
     ADDRESSES = [0x77, 0x76]
     I2C = busio.I2C(board.SCL, board.SDA)
 
-    @classmethod
-    def probe(cls, send_payload_callback=None) -> list:
+    @staticmethod
+    def functional_probe(cls, send_payload_callback=None, lock=None):
         """
         Probe for Bosch BME type sensors to the possible I2C addresses.
 
-        Function which iterates over multiple I2C addresses and look for
-        sensors of the type BoschBME. In case the sensor is not found at any
-        of the specified I2C address, None is returned.
+        Function which iterates over multiple I2C addresses and looks for sensors of the type
+        BoschBME. In case the sensor is not found at the specified I2C address, None is returned.
         """
 
-        lock_file = "/var/lock/hsh_" + cls.get_sensor_name() + ".lock"
-        lock = FileLock(lock_file)
-
-        with lock:
+        for address in cls.ADDRESSES:
             try:
-                os.chmod(lock_file, 0o777)
-            except PermissionError:
-                pass
-
-            for address in ProbeBoschBME680.ADDRESSES:
-                try:
-                    sensor = cls.get_sensor_probe_function()(ProbeBoschBME680.I2C, address)
-                    logger.success("{} found at {}".format(cls.get_sensor_name(), hex(address)))
-                    # Initial read to re-initialize sensor.
-                    # Fixes bad state when it is run in multiple instances
-                    # ... sometimes ...
-                    sensor.gas
-                    sensor.temperature
-                    sensor.humidity
-                    sensor.pressure
-                    return cls.generate_sensor_types(sensor, send_payload_callback, lock)
-                except ValueError:
-                    logger.verbose("Found no {} sensor at address {}."
-                                   .format(cls.get_sensor_name(), hex(address)))
-                except RuntimeError:
-                    logger.verbose("The chip found at {} address has a different ID than {}."
-                                   .format(hex(address), cls.get_sensor_name()))
-                    logger.verbose("These are not the sensors you're looking for.")
+                sensor = cls.get_sensor_probe_function()(cls.I2C, address)
+                logger.success("{} found at {}".format(cls.get_sensor_name(), hex(address)))
+                # Initial read to re-initialize sensor.
+                # Fixes bad state when it is run in multiple instances
+                # ... sometimes ...
+                sensor.temperature
+                sensor.humidity
+                sensor.pressure
+                return cls.generate_sensor_types(sensor, send_payload_callback, lock)
+            except ValueError:
+                logger.verbose("Found no {} sensor at address {}."
+                               .format(cls.get_sensor_name(), hex(address)))
+            except RuntimeError:
+                logger.verbose("The chip found at {} address has a different ID than {}."
+                               .format(hex(address), cls.get_sensor_name()))
+                logger.verbose("These are not the sensors you're looking for.")
 
     @staticmethod
     def get_sensor_probe_function():
--- homesensorhub/sensors/TSL258x/probe.py
+++ homesensorhub/sensors/TSL258x/probe.py
@@ -1,8 +1,6 @@
 from homesensorhub.sensors.TSL258x.driver import TSL258x
 from homesensorhub.sensors.TSL258x.type import LightTSL258x
 from homesensorhub.sensors.probe import Probe
-from filelock import FileLock
-import os
 
 from homesensorhub.utils import logging
 logger = logging.getLogger(__name__)
@@ -12,25 +10,18 @@ class ProbeTSL258x(Probe):
 
     SENSOR_NAME = 'TSL258x'
 
-    @classmethod
-    def probe(cls, send_payload_callback=None) -> list:
-        """Probe board for TSL258x sensor."""
+    @staticmethod
+    def functional_probe(cls, send_payload_callback=None, lock=None):
+        sensor = TSL258x.probe()
+        sensor.config()
 
-        lock_file = "/var/lock/hsh_" + cls.SENSOR_NAME + ".lock"
-        lock = FileLock(lock_file)
-
-        with lock:
-            try:
-                os.chmod(lock_file, 0o777)
-            except PermissionError:
-                pass
-
-            sensor = TSL258x.probe()
-            sensor.config()
-
-        logger.debug("Found {} sensor.".format(cls.SENSOR_NAME))
+        logger.debug("Found {} sensor.".format(cls.get_sensor_name()))
 
         return cls.generate_sensor_types(sensor, send_payload_callback, lock)
+
+    @staticmethod
+    def get_sensor_name():
+        return 'TSL258x'
 
     def generate_sensor_types(sensor, send_payload_callback, lock):
         return [LightTSL258x(sensor, send_payload_callback, lock)]
